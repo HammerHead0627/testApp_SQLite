d519c1895a09f9d93fa260d954beb145862c02c5
HammerHead <legox06270627@gmail.com>
2021-04-03 08:21:05 +0900

    edit onCreate add readData

--- app/src/main/java/com/example/testapp_sqlite/MainActivity.java
+++ app/src/main/java/com/example/testapp_sqlite/MainActivity.java
@@ -3,17 +3,60 @@ package com.example.testapp_sqlite;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView productListView;
+    private TestOpenHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // DB作成
+        helper = new TestOpenHelper(getApplicationContext());
+
+        // ListViewに表示するリスト項目をArrayListで準備する
+        //TODO DBから追加した品目と値段を追加する
+        ArrayList shoppingData = new ArrayList<>();
+
+        // リスト項目とListViewを対応付けるArrayAdapterを用意する
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,shoppingData);
+
+        // ListViewにArrayAdapterを設定する
+        ListView listView = (ListView)findViewById(R.id.shoppingList);
+        listView.setAdapter(adapter);
+
     }
+
+    /**
+     * DBからデータを全件取得し画面に表示する.
+     * @param view
+     */
+    public void readData(View view) {
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor cursor = db.query(
+                "shoppingData",
+                new String[]{"product","amount","quantity"},
+                null
+                ,null
+                ,null
+                ,null
+                ,null
+        );
+
+    }
+
 }
--- app/src/main/java/com/example/testapp_sqlite/TestOpenHelper.java
+++ app/src/main/java/com/example/testapp_sqlite/TestOpenHelper.java
@@ -1,9 +1,69 @@
 package com.example.testapp_sqlite;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class TestOpenHelper extends SQLiteOpenHelper {
+    // データーベースのバージョン
+    private static final int DATABASE_VERSION = 3;
 
+    // データーベース情報を変数に格納
+    private static final String DATABASE_NAME = "ShoppingDB.db";
+    private static final String TABLE_NAME = "products";
+    private static final String _ID = "_id";
+    private static final String COLUMN_NAME_PRODUCT  = "product";
+    private static final String COLUMN_NAME_AMOUNT = "amount";
+    private static final String COLUMN_NAME_QUANTITY = "quantity";
+
+    //テーブル作成SQL
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    _ID + " INTEGER PRIMARY KEY," +
+                    COLUMN_NAME_PRODUCT + " TEXT," +
+                    COLUMN_NAME_AMOUNT + " INTEGER," +
+                    COLUMN_NAME_QUANTITY + "INTEGER)";
+
+    //同名テーブル消去SQL
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    TestOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(
+                SQL_CREATE_ENTRIES
+        );
+        //初期データをDBに追加する
+        saveData(db, "コーラ", 100 ,1);
+
+    }
+
+    //おまじない
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(
+                SQL_DELETE_ENTRIES
+        );
+        onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+
+    //データをproductsテーブルに追加する
+    public void saveData(SQLiteDatabase db, String product, int amount ,int quantity){
+        ContentValues values = new ContentValues();
+        values.put("title", product);
+        values.put("score", amount);
+        values.put("quantity", quantity);
+
+        db.insert("products", null, values);
+    }
 }
