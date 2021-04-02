036ab0986017846088d165f9b73d3a84b97c23bd
HammerHead <legox06270627@gmail.com>
2021-04-03 03:13:46 +0900

    first commit

---
+++ app/src/main/java/com/example/testapp_sqlite/TestOpenHelper.java
@@ -0,0 +1,9 @@
+package com.example.testapp_sqlite;
+
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class TestOpenHelper extends SQLiteOpenHelper {
+
+}
